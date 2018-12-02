c8a87e654bc3d2685e32d6f36157a8113db0a14d
sherly <sher.t92@gmail.com>
2018-12-02 18:02:59 +0100

    Refactore requestMove and add move function

--- game.c
+++ game.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int move(int pos, int player, int * cells);
+void move(int pos, int player, int * cells, int seeds_computer, int seeds_player);
+int requestMove(int player);
 
 void printBoard(int * b, int seeds_computer, int seeds_player) {
-    printf("Computer side\n");
+    printf("\nComputer side\n");
     printf("         0     1     2     3     4     5\n");
     printf("-------------------------------------------------\n");
     printf("|%5d|%5d|%5d|%5d|%5d|%5d|%5d|%5s|\n", seeds_computer, b[0], b[1], b[2], b[3], b[4], b[5], "");
@@ -12,7 +13,7 @@ void printBoard(int * b, int seeds_computer, int seeds_player) {
     printf("|%5s|%5d|%5d|%5d|%5d|%5d|%5d|%5d|\n", "", b[11], b[10], b[9], b[8], b[7], b[6], seeds_player);
     printf("-------------------------------------------------\n");
     printf("         11    10    9     8     7     6\n");
-    printf("Player side\n");
+    printf("Player side\n\n");
 }
 
 int main(void) {
@@ -22,23 +23,47 @@ int main(void) {
     int cells[12] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
     int seeds_player = 0;
     int seeds_computer= 0;
-    int position;
+    int position; // fix this
 
-    for (int i=0; i<12; i++) {
-        printf("Cell values: %d.\n", cells[i]);
-    }
+    printBoard(cells, seeds_computer, seeds_player);
+
     int player; // 0 represents comp, 0 represents player
     printf("Who starts? 0: computer, 1: player.\n");
     scanf("%d", &player);
 
+    position = requestMove(player);
+
+    move(position, player, cells, seeds_computer, seeds_player);
+    return 0;    
+}
+
+int requestMove(int player) {
+    int pos;
+
     if (player == 0) {
-        printf("Which position to sow? Between 0 to 5.\n");
-        scanf("%d", &position);
-    } else if (player == 1) {
-        printf("Which position to sow? Between 6 to 11.\n");
-        scanf("%d", &position);
+        do {
+            printf("Which position to sow? Between 0 to 5.\n");
+            scanf("%d", &pos);
+        } while(!(pos >= 0 && pos <= 5));
+    } else {
+        do {
+            printf("Which position to sow? Between 6 to 11.\n");
+            scanf("%d", &pos);
+        } while(!(pos >= 6 && pos <= 11));
+    }
+
+    return pos;
+}
+
+void move(int pos, int player, int * cells, int seeds_computer, int seeds_player) {
+    int stones = cells[pos];
+    cells[pos] = 0;
+
+    for (int i = 0; i < stones; i++) {
+        int idx = (pos + i + 1) % 12;
+        // add in logic to skip the original pos
+        cells[idx] += 1;
     }
 
     printBoard(cells, seeds_computer, seeds_player);
-    return 0;    
 }
